c2bd38d67081668e16ca2bd1a71a1b8dbb02b0d5
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2025-06-06 16:39:53 +0530

    backend connected

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:8080/api/v1"
-      : "/api/v1",
+      : "https://love-leetcode-backend.onrender.com/api/v1",
   withCredentials: true,
 });
