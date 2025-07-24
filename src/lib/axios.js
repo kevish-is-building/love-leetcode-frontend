620c05b2118bb176e5e33622241472ad04ce8305
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2025-07-24 18:18:55 +0530

    running localhost

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.MODE === "development"
-      ? "https://love-leetcode-backend.onrender.com/api/v1"
-      : "https://love-leetcode-backend.onrender.com/api/v1",
+    import.meta.env.MODE === "production"
+      ? import.meta.env.BACKEND_URL
+      : "http://localhost:8080/api/v1",
   withCredentials: true,
 });
