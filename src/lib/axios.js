4e46db4a50a588399a4bc8bec8fca97cbd163259
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2025-07-24 18:34:21 +0530

    env changes

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "production"
-      ? import.meta.env.BACKEND_URL
+      ? `${import.meta.env.BACKEND_URL}/api/v1`
       : "http://localhost:8080/api/v1",
   withCredentials: true,
 });
