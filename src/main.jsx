c80b4210e9c1d161fd739b930c9d4a58de3c036c
Kevish Sewliya <129868668+Kevish07@users.noreply.github.com>
2025-08-26 10:21:44 +0530

    vercel injection

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { inject } from "@vercel/analytics";
 import "./index.css";
 import App from "./App.jsx";
 
+inject();
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="202580823383-lhfq9robejoj8i9vsg73d8jfnii4o7da.apps.googleusercontent.com">
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </GoogleOAuthProvider>
   </StrictMode>,
 );
